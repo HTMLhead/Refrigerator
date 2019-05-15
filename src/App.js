48426f2806ac654039d2dda1a675f886ffd2da78
HTMLhead <mrdoo309@gmail.com>
2019-05-15 22:57:20 +0900

    Feat: 합쳐주는 App.js파일에 만든 레이아웃 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoAppWrapper from './component/presenter/TodoAppWrapper';
+import Header from './component/presenter/Header';
+import TodoListWrapper from './component/presenter/TodoListWrapper';
+import TodoNavWrapper from './component/presenter/TodoNavWrapper';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <TodoAppWrapper>
+        <TodoNavWrapper />
+        <TodoListWrapper />
+      </TodoAppWrapper>
+    </>
   );
 }
 
