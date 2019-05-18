a5f183f5ac875da96240e3d407aebff3a2ae995d
HTMLhead <mrdoo309@gmail.com>
2019-05-18 15:44:01 +0900

    Feat: icon적용 및, content를 담아주는 span element제작

--- src/component/presenter/wrappers/TodoNav.jsx
+++ src/component/presenter/wrappers/TodoNav.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import NavBtn from '../buttons/NavBtn';
+import DoingIcon from '../icons/DoingIcon';
+import DoneIcon from '../icons/DoneIcon';
 
 const TodoNavWrapper = styled.div`
   padding-top: 10%;
@@ -10,12 +12,24 @@ const TodoNavWrapper = styled.div`
   height: 90vh;
   background-color: #272727;
 `;
+const NavBtnContent = styled.span`
+  font-size: 1.7rem;
+  display: flex;
+  align-items: center;
+  margin-left: 5%;
+`;
 
 const TodoNav = () => {
   return (
     <TodoNavWrapper>
-      <NavBtn>할 일</NavBtn>
-      <NavBtn>한 일</NavBtn>
+      <NavBtn>
+        <DoingIcon />
+        <NavBtnContent>할 일</NavBtnContent>
+      </NavBtn>
+      <NavBtn>
+        <DoneIcon />
+        <NavBtnContent>한 일</NavBtnContent>
+      </NavBtn>
     </TodoNavWrapper>
   );
 };
