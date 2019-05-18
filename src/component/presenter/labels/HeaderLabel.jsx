7be236a9c288533835b78c839293b1546c258c6a
HTMLhead <mrdoo309@gmail.com>
2019-05-18 14:54:05 +0900

    Feat: 헤더에 들어갈 메인 라벨 제작

---
+++ src/component/presenter/labels/HeaderLabel.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const HeaderLabel = styled.div`
+  margin: auto;
+  color: #fff;
+  font-size: 2rem;
+  font-weight: bold;
+`;
+
+export default HeaderLabel;
