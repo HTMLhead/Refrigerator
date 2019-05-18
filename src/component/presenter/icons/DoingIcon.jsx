28397e78a48dc0317237f42d0d4e3561d87b8aa2
HTMLhead <mrdoo309@gmail.com>
2019-05-18 15:42:31 +0900

    Feat: 할일과 한 일의 아이콘 추가

---
+++ src/component/presenter/icons/DoingIcon.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const DoingIcon = styled.div`
+  padding: 5%;
+  border: 0.3rem solid #fff;
+  border-radius: 5px;
+  margin: 1%;
+`;
+
+export default DoingIcon;
---
+++ src/component/presenter/icons/DoneIcon.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const DoneIcon = styled.div`
+  padding: 5%;
+  position: relative
+  border: 0.3rem solid #fff;
+  border-radius: 5px;
+  margin: 1%;
+  &::after {
+    content: '';
+    position :absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%) rotate(45deg);
+    border-radius: 3px;
+    padding: 20%;
+    border-bottom: 0.3rem solid #fff;
+    border-right: 0.3rem solid #fff
+  }
+`;
+
+export default DoneIcon;
