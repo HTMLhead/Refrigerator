4c9cd4c0c74e563f1a5299d2e7074fd86bea8d5b
HTMLhead <mrdoo309@gmail.com>
2019-05-18 15:13:28 +0900

    Feat: navigator버튼 제작

---
+++ src/component/presenter/buttons/NavBtn.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const NavBtn = styled.div`
+  padding: 5%;
+  padding-left: 20%;
+  width: 100%;
+  font-size: 1.3rem;
+  color: #fff;
+  curser: pointer;
+  &:hover {
+    trainsition: background;
+    background-color: #363636;
+    font-weight: bold;
+  }
+`;
+
+export default NavBtn;
